8c118d302d4becbf34194118d4eba76775f5626e
bababadboy <sc30logo@gmail.com>
2018-11-27 16:24:33 +0800

    Add @ElementCollection

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO product
-VALUES(NULL,"YK070005B","电脑桌",230,1,"桌子","放电脑的桌子",233.33,"https://s1.ax1x.com/2018/11/20/F9MJJK.jpg");
-
-INSERT INTO product
-VALUES(NULL,"YG073005B","电脑桌",200,1,"桌子","放电脑的桌子",250.33,"https://s1.ax1x.com/2018/11/20/F9MJJK.jpg");
