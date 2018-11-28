650bfc0216efabf5b34132499dd862d6559ffc6d
Ash_zd <ash_zd@qq.com>
2018-11-28 20:55:02 +0800

    temp commit

--- src/main/java/com/bababadboy/dealermng/controller/DealerController.java
+++ src/main/java/com/bababadboy/dealermng/controller/DealerController.java
@@ -1,13 +1,32 @@
 package com.bababadboy.dealermng.controller;
 
-import org.springframework.stereotype.Controller;
+import com.alibaba.fastjson.JSON;
+import com.bababadboy.dealermng.entity.Dealer;
+import com.bababadboy.dealermng.repository.DealerRepository;
+import com.bababadboy.dealermng.service.DealerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Ash
  */
-@Controller
-@RequestMapping(value = "/dealer")
+@RestController
 public class DealerController {
+    @Autowired
+    private DealerService dealerService;
+
+    @Autowired
+    private DealerRepository dealerRepository;
+
+    @RequestMapping(value = "/dealer", method = RequestMethod.GET)
+    public Object listDealer() {
+        List<Dealer> dealers = dealerRepository.findAll();
+        Object json = JSON.toJSON(dealers);
+        return json;
+    }
 
 }
