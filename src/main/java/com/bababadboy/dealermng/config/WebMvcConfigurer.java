0bb1f176085fa760f8e81871e516368fe6044070
Ash_zd <ash_zd@qq.com>
2018-12-02 23:11:56 +0800

    解决报错'Content-Type' cannot contain wildcard type '*'

--- src/main/java/com/bababadboy/dealermng/config/WebMvcConfigurer.java
+++ src/main/java/com/bababadboy/dealermng/config/WebMvcConfigurer.java
@@ -2,9 +2,11 @@ package com.bababadboy.dealermng.config;
 
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,12 +17,33 @@ import java.util.List;
 public class WebMvcConfigurer extends WebMvcConfigurationSupport {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        //自定义配置...
-        //FastJsonConfig config = new FastJsonConfig();
-        //config.set ...
-        //converter.setFastJsonConfig(config);
-
+        /*
+          解决报错
+          org.springframework.web.util.NestedServletException:
+          Request processing failed; nested exception is java.lang.IllegalArgumentException:
+          'Content-Type' cannot contain wildcard type '*'
+         */
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XML);
+        supportedMediaTypes.add(MediaType.IMAGE_GIF);
+        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+        supportedMediaTypes.add(MediaType.IMAGE_PNG);
+        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+        supportedMediaTypes.add(MediaType.TEXT_XML);
+        converter.setSupportedMediaTypes(supportedMediaTypes);
         converters.add(0, converter);
     }
 }
