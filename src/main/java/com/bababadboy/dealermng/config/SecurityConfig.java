1c76db14cc80f77bd4c962abeb62b525295c7f22
Ash_zd <ash_zd@qq.com>
2018-12-02 23:09:32 +0800

    暂时禁用csrf

--- src/main/java/com/bababadboy/dealermng/config/SecurityConfig.java
+++ src/main/java/com/bababadboy/dealermng/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/*").permitAll();
+        http.csrf().disable();
     }
 
 }
