62d6cca3cbcd44ff377cc5f9287bab5c28ea520d
Ash_zd <ash_zd@qq.com>
2018-12-02 23:21:18 +0800

    经销商增删改查暂时完成

--- src/main/java/com/bababadboy/dealermng/repository/DealerRepository.java
+++ src/main/java/com/bababadboy/dealermng/repository/DealerRepository.java
@@ -2,10 +2,12 @@ package com.bababadboy.dealermng.repository;
 
 import com.bababadboy.dealermng.entity.Dealer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Ash
  */
+@Repository
 public interface DealerRepository extends JpaRepository<Dealer, Long> {
 
 }
--- src/main/java/com/bababadboy/dealermng/service/DealerServiceImpl.java
+++ src/main/java/com/bababadboy/dealermng/service/DealerServiceImpl.java
@@ -1,16 +1,33 @@
 package com.bababadboy.dealermng.service;
 
+import com.bababadboy.dealermng.entity.Dealer;
+import com.bababadboy.dealermng.pojo.Address;
+import com.bababadboy.dealermng.pojo.Gender;
 import com.bababadboy.dealermng.repository.DealerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Field;
+import java.util.*;
 
 /**
  * @author Ash
  */
 @Service
 public class DealerServiceImpl implements DealerService {
+    private final DealerRepository dealerRepository;
+
     @Autowired
-    private DealerRepository dealerRepository;
+    public DealerServiceImpl(DealerRepository dealerRepository) {
+        this.dealerRepository = dealerRepository;
+    }
 
-
+    @Override
+    public Page<Dealer> listDealersByPage(Integer page, Integer size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return dealerRepository.findAll(pageable);
+    }
 }
