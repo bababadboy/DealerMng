18334d7b74f7cfa2d7ecd44c27fce19020b1d8fd
bababadboy <sc30logo@gmail.com>
2018-12-03 16:54:25 +0800

    Add productRepoCustom

---
+++ src/main/java/com/bababadboy/dealermng/entity/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.bababadboy.dealermng.entity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.ws.Response;
+
+
+/**
+ * 描述:
+ * 全局异常处理类
+ *
+ * @author wangxiaobin
+ * @create 2018-12-02 下午3:51
+ */
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 处理所有不可知的异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    ResponseEntity.BodyBuilder handleException(Exception e){
+
+        LOGGER.error(e.getMessage(), e);
+        return ResponseEntity.status(500);
+    }
+
+    /**
+     * 处理所有业务异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(BusinessException.class)
+    @ResponseBody
+    ResponseEntity.BodyBuilder handleBusinessException(BusinessException e){
+        LOGGER.error(e.getMessage(), e);
+
+
+        return ResponseEntity.status(500);
+    }
+
+
+}
+
