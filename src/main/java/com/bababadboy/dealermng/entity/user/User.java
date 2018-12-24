c36e126238339f933daa5c06fa60af510db7337e
Ash_zd <ash_zd@qq.com>
2018-12-24 23:01:52 +0800

    fix utc problem

--- src/main/java/com/bababadboy/dealermng/entity/user/User.java
+++ src/main/java/com/bababadboy/dealermng/entity/user/User.java
@@ -16,6 +16,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
     @Size(min = 4, max = 255, message = "Minimum username length: 4 characters")
